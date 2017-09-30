07ca1149555c4a6852c6269ae27c952999224169
Piotr Czajka <piotr_czajka@outlook.com>
2017-09-30 15:27:49 +0200

    Almost done, no support for to less or to much images, no support for up/down monitors, width resolutions problem

--- lock.py
+++ lock.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import PIL
 import re
 import subprocess
 import os
+import imghdr
+from PIL import Image
 
 IMAGE_FOLDER = 'images'
 
@@ -15,16 +16,61 @@ def get_monitor_resolutions():
     connected_lines = [line for line in lines if
                        re.search('\sconnected\s', line)]
 
-    ret = []
+    res = []
     for line in connected_lines:
         match = re.search('\d+x\d+', line)
-        ret.append(match.group(0))
-    return tuple(ret)
+        res.append(match.group(0))
+    str_res = tuple([x.split('x') for x in res])
+    return [tuple([int(x), int(y)]) for x, y in str_res]
 
+
+def asert_only_pngs():
+    for filename in os.listdir(IMAGE_FOLDER):
+        ftype = imghdr.what(os.path.join(IMAGE_FOLDER, filename))
+        if ftype != 'png':
+            raise Exception('All files in image folder must be png format')
+
+
+def crop_images_to_resolutions(resolutions):
+    images = os.listdir(IMAGE_FOLDER)
+    if len(images) != len(resolutions):
+        raise Exception('There should be the same number of images,'
+                        'as monitors')
+    ret = []
+    for image, resolution in zip(os.listdir(IMAGE_FOLDER), resolutions):
+        f = Image.open(os.path.join(IMAGE_FOLDER, image))
+        f.resize(resolution)
+        ret.append(f)
+    return ret
+
+
+def get_fi_res(images):
+    return tuple([sum([image.size[0] for image in images]),
+                 get_monitor_resolutions()[0][1]])
+
+
+def combine_images(images):
+    final_image_resolution = get_fi_res(images)
+    final_image = Image.new(images[0].mode, final_image_resolution)
+    sum_ = 0
+    for image in images:
+        final_image.paste(image, (sum_, 0))
+        sum_ += image.size[0]
+    return final_image
 
 
 def main():
     resolutions = get_monitor_resolutions()
+    print(resolutions)
+    asert_only_pngs()
+    images = crop_images_to_resolutions(resolutions)
+    image = combine_images(images)
+    for i in images:
+        i.close()
+    image.save('/tmp/background.png')
+    image.close()
+    subprocess.call('i3lock -i /tmp/background.png', shell=True)
+
 
 if __name__ == '__main__':
     main()
